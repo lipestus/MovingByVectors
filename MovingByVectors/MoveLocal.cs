351b6580a6943ac4161a7b1c7fcb68a2872e9f0f
Felipe Ferrao <35579069+lipestus@users.noreply.github.com>
2018-05-25 16:40:39 +0100

    Add files via upload

---
+++ MovingByVectors/MoveLocal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour {
+
+    public Transform goal;
+    private float speed = 0f;
+    private float accuracy = 2f;
+    private float rotationSpeed = 2f;
+    private float dist;
+    float delay = 1.0f;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        anim.SetFloat("speed", speed);
+        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+        dist = Vector3.Distance(transform.position, lookAtGoal);
+        Vector3 direction = lookAtGoal - this.transform.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSpeed);
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+
+        if (dist > accuracy)
+        {
+            StartCoroutine(DelayToWalk(delay));
+        }
+
+        else
+        {
+            speed = 0;
+        }
+
+        print(dist);
+    }
+
+    public IEnumerator DelayToWalk(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        speed = 1f;
+        if(speed != 0)
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ MovingByVectors/WaypointsNavigation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsNavigation : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    int currentWaypoint = 0;
+    private Animator anim;
+
+    float speed = 1.5f;
+    float accuracy = 1f;
+    float rotationSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        waypoints = GameObject.FindGameObjectsWithTag("waypoints");
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (waypoints.Length == 0)
+            return;
+
+        Vector3 lookAtGoal = new Vector3(waypoints[currentWaypoint].transform.position.x, this.transform.position.y, waypoints[currentWaypoint].transform.position.z);
+        Vector3 direction = lookAtGoal - this.transform.position; // get magnitude
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSpeed);
+        Debug.DrawRay(this.transform.position, direction, Color.green);
+
+        if(direction.magnitude < accuracy)
+        {
+            currentWaypoint++;
+            if(currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+
+        anim.SetFloat("speed", speed);
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+}
